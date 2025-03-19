912d09dc34f5f14c22536f8e0582d83df1821ed1
MORRIS EBITIMI JOSEPH <myrrisbank@gmail.com>
2025-03-19 09:44:06 +0100

    Update eslint.config.mjs

--- Fundraising_Dapp/front-end/eslint.config.mjs
+++ Fundraising_Dapp/front-end/eslint.config.mjs
@@ -1,16 +1,16 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  env: {
+    NEXT_PUBLIC_STACKS_NETWORK: process.env.NEXT_PUBLIC_STACKS_NETWORK,
+    NEXT_PUBLIC_DEVNET_HOST: process.env.NEXT_PUBLIC_DEVNET_HOST,
+    NEXT_PUBLIC_PLATFORM_HIRO_API_KEY: process.env.NEXT_PUBLIC_PLATFORM_HIRO_API_KEY,
+    NEXT_PUBLIC_CONTRACT_DEPLOYER_TESTNET_ADDRESS: process.env.NEXT_PUBLIC_CONTRACT_DEPLOYER_TESTNET_ADDRESS,
+    NEXT_PUBLIC_CONTRACT_DEPLOYER_MAINNET_ADDRESS: process.env.NEXT_PUBLIC_CONTRACT_DEPLOYER_MAINNET_ADDRESS,
+  },
+  eslint: {
+    ignoreDuringBuilds: true, // Ignore ESLint during the build process
+  },
+  distDir: 'build', // Specify custom build directory
+};
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
-
-export default eslintConfig;
+export default nextConfig;
